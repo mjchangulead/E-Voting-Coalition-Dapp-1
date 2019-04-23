36aeb455ffae625a18006fcc99f9b3210350c8d8
Chang Mei-Chih <37042542+MeiChihChang@users.noreply.github.com>
2019-04-23 08:26:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider} from 'react-redux';
-import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {submitShowIdea } from './reducers';
 import 'tachyons';
 
-const store = createStore(submitShowIdea);
-
-ReactDOM.render(
-                <Provider store={store}>
-                <App />
-                </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
